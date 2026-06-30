f575f0a29da7b3d7d9afcd33c07f6e7c43bf87d6
Monti <montaser.dv@email.com>
2026-06-30 10:05:56 +0300

    v3

--- git_deploy/index.php
+++ git_deploy/index.php
@@ -1,8 +1,6 @@
 <?php
 
-//xxxx_hh_
-
-   $secret = "105504801_170001144_147710923"; // <-- نفس السر في GitHub
+   $secret = "105504801_170001144_147715555"; // <-- نفس السر في GitHub
 
       // استقبال توقيع GitHub
       $header = $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
@@ -15,36 +13,36 @@
       // تحقق من التوقيع
       if (!hash_equals($calculated, $header)) {
 
-          file_put_contents("/home/demo.trakmile.com/public_html/deploy/deploy_error.log",
+          file_put_contents("/home/trakmile.com/public_html/git_deploy/deploy_error.log",
               "Invalid Signature\n".
               "Received: $header\n".
               "Expected: $calculated\n\n",
               FILE_APPEND
           );
 
-          http_response_code(403);
+          //http_response_code(403);
           //exit("Invalid signature");
         $result="Invalid signature";
       }
       // نفّذ ملف التحديث
 
-      $result = shell_exec("/bin/git -c safe.directory=/home/demo.trakmile.com/public_html -C /home/demo.trakmile.com/public_html pull origin main 2>&1");
-      $result = shell_exec("/bin/git -c safe.directory=/home/demo.trakmile.com/public_html -C /home/demo.trakmile.com/public_html reset --hard origin/main 2>&1");
+      $result = shell_exec("/usr/bin/git -c safe.directory=/home/trakmile.com/public_html -C /home/trakmile.com/public_html reset --hard origin/main 2>&1");
+      $result = shell_exec("/usr/bin/git -c safe.directory=/home/trakmile.com/public_html -C /home/trakmile.com/public_html pull origin main 2>&1");
+      
       
 
-      //shell_exec("bash /home/demo.trakmile.com/public_html/deploy/deploy.sh 2>&1");
-      //file_put_contents("/home/demo.trakmile.com/public_html/deploy/deploy.log", print_r($result, true));
+
 
       $resultx = shell_exec("
 whoami
 pwd
-ls -la /home/demo.trakmile.com/public_html/.git
+ls -la /home/trakmile.com/public_html/.git
 /bin/git --version
 2>&1
 ");
 
 file_put_contents(
-    '/home/demo.trakmile.com/public_html/deploy/deploy.log',
+    '/home/trakmile.com/public_html/git_deploy/deploy.log',
     "\n====================\n" .
     date('Y-m-d H:i:s') . "\n" .
     $resultx .
