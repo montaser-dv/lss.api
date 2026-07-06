0986f2ee51f1dcecf1aa2ac0e8cbee33ca9c9cd5
Monti <montaser.dv@email.com>
2026-07-06 10:44:33 +0300

    m13

--- mobile-api/config.php
+++ mobile-api/config.php
@@ -2,4 +2,4 @@
 
 require_once dirname(__DIR__) . '/debug.php';
 
-$mainDB = new mysqli('localhost', 'lss_main_user', '905DY2KpP60WfvDb', 'lss_main');
+$mainDB = new mysqli('localhost', 'trak_user', '1M^9n1P-Oj3j90JE', 'trak_db');
